872eabecbed5c93ac12c35bda24886525c113a76
RBG74 <remy.bernardgranger@gmail.com>
2018-05-31 09:45:12 +0200

    Done

--- callbacks.js
+++ callbacks.js
@@ -1,22 +1,37 @@
-function resizeImages(source, dest, width, height, callback) {
-    fs.readdir(source, (err, files) => {
-        if (err) {
-            callback(err);
-        } else {
-            files.forEach((filename, fileIndex) => {
-                gm(source + filename)
-                    .resize(width, height)
-                    .write(
-                        dest + "w" + width + "_" + filename,
-                        (err, values) => {
-                            if (err) {
-                                callback(err);
-                            } else if (fileIndex == files.length - 1) {
-                                callback();
-                            }
-                        }
-                    );
-            });
-        }
-    });
+function resizeImages(source, dest, width, height) {
+    return new Promise((resolve, reject) => {
+        fs.readdir(source, (err, files) => {
+            if (err) {
+                reject(err);
+            } else {
+                const promises = [];
+                files.forEach((filename) => {
+                    promises.push(resizeImage(source, filename, width, height));
+                });
+                Promise.all(promises)
+                    .then((values) => {
+                        resolve(values);
+                    })
+                    .catch((err) => {
+                        reject(err);
+                    });
+            }
+        })
+    }
+}
+
+function resizeImage(source, filename, width, height){
+    return new Promise((resolve, reject) => {
+        gm(source + filename)
+            .resize(width, height)
+            .write(
+                dest + "w" + width + "_" + filename,
+                (err, values) => {
+                    if (err) {
+                        reject(err);
+                    }
+                    resolve(values);
+                }
+            );
+    }
 }
